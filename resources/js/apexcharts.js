800589e5d09ac51c76472e210000d5a893014a37
Alvien <alvien.salvador@levno.com>
2025-11-25 09:57:21 +1300

    fix: add npm projects

--- resources/js/apexcharts.js
+++ resources/js/apexcharts.js
@@ -1,4 +1,3 @@
-
 import ApexCharts from "apexcharts";
 
 window.renderApexChart = function (chartId, options) {
--- resources/views/livewire/components/graph.blade.php
+++ resources/views/livewire/components/graph.blade.php
@@ -1,48 +1,30 @@
-
-{{-- <h1 class="">
-    {{ $title }}
-</h1> --}}
-
 <div class="h-72">
-
-
     <div id="{{ $chartId }}"></div>
-
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-
-<script>
-
-   var options = {
-          series: [{
-            name: @json($data['name']),
-            data: @json($data['data'])
-        }],
-          chart: {
-          type: @json($type),
-          height: '100%',
-          width: '100%',
-          },
-        dataLabels: {
-          enabled: false
-        },
-       
-        xaxis: {
-          categories: @json($labels),
-          labels: {
-            show: false
-          }
-        },
-        title:{
-            text: @json($title),
-            align: 'left'
+<script type="module">
+    renderApexChart(
+        @json($chartId),
+        {
+            series: [{
+                name: @json($data['name']),
+                data: @json($data['data'])
+            }],
+            chart: {
+                type: @json($type),
+                height: "100%",
+                width: "100%",
+            },
+            dataLabels: { enabled: false },
+            xaxis: {
+                categories: @json($labels),
+                labels: { show: false }
+            },
+            title: {
+                text: @json($title),
+                align: 'left'
+            }
         }
-        };
-
-        // console.log(json($data));
-
-        var chart = new ApexCharts(document.querySelector("#"+@json($chartId)), options);
-        chart.render();
-    
+    );
 </script>
+
